305a97e7b3f379b264525c6300990224f9d6d52f
Max Ivanov <maximus-dr@mail.ru>
2021-02-09 23:45:28 +0300

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div className="container pt-4">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import React from 'react';
+import './index.scss';
+
 
 ReactDOM.render(
   <React.StrictMode>
